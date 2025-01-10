3cc43df232b048237afc154b7deb181a8b71d988
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2025-01-10 04:02:28 -0800

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,29 +1,25 @@
 #include <string>
 #include <vector>
 #include <queue>
-
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    queue<int> remaindays;
+    queue<int> q;
     for(int i=0;i<progresses.size();i++){
-        int days=(100-progresses[i]+speeds[i]-1)/speeds[i] ;
-        remaindays.push(days);
+    int remain_d=(100-progresses[i]+speeds[i]-1)/speeds[i];
+        q.push(remain_d);
     }
-    while(!remaindays.empty()){
-        int curdays=remaindays.front();
-        remaindays.pop();
+    while(!q.empty()){
+        int cur=q.front();
+        q.pop();
         int cnt=1;
-        while(!remaindays.empty()&&remaindays.front()<=curdays){
+        while(!q.empty()&&q.front()<=cur){
             cnt++;
-            remaindays.pop();
+            q.pop();
         }
         answer.push_back(cnt);
+        
     }
     return answer;
- 
 }
-/*
-100-progress/speed ==days
-*/

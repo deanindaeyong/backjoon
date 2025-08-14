927a339b502fb321cb3082f4b7a46883892b25af
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2025-08-14 18:20:25 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,30 +1,30 @@
 #include <iostream>
 using namespace std;
+int point[305];
 int n;
-int arr[301][301];
-int point[301];
+int arr[305];
+
 int main() 
-{
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin>>n;
-  
-  for(int i=1;i<=n;i++){
-    cin>>point[i];
-  }
-  arr[0][1]=0;
-  arr[0][2]=0;
-  arr[1][1]=point[1];
-  arr[1][2]=point[1];
-  arr[2][1]=point[2];
-  arr[2][2]=point[1]+point[2];
-  for(int i=3;i<=n;i++){
-    arr[i][1]=max(arr[i-2][2],arr[i-2][1])+point[i];
-    arr[i][2]=arr[i-1][1]+point[i];
-    
-  }
-  cout<<max(arr[n][1],arr[n][2]);
-  
-  
+{   ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n;
+    int total=0;
+    for(int i=1;i<=n;i++){
+      cin>>point[i];
+      total+=point[i];
+    }
+    if(n<=2){
+      cout<<total;
+      return 0;
+      
+    }
+    arr[1]=point[1];
+    arr[2]=point[2];
+    arr[3]=point[3];
+    for(int i=4;i<=n-1;i++){
+      arr[i]=min(arr[i-2],arr[i-3])+point[i];
+      
+    }
+    cout<<total-min(arr[n-1],arr[n-2]);
     return 0;
 }

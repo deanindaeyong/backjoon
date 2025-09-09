a2c8d1fad7a2f5b7f3eb35e2fe28fde8bf08ff81
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2025-09-09 22:18:04 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,21 +1,32 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 string a;
 int l;
+long long m=1234567891;
+long long power(long long a, long long b){
+ if(b==1){
+   return a;
+ }else if(b==0){
+   return 1;
+ }
+ return a*power(a,b-1)%m;
+}
 int main() 
 {   cin>>l;
     cin>>a;
-    int r=31;
-    int m=1234567891;
-    int sum=0;
+    long long r=31;
+    
+    long long sum=0;
     
     for(int i=0;i<l;i++){
-      sum=sum+(a[i]-'a'+1)*(pow(r,i));
- 
+      sum=(sum+(a[i]-'a'+1)*(power(r,i)))%m;
+      
     }
    
     cout<<sum;
+  //cout<<power(4,4);
     
     return 0;
 }
